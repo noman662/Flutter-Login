d8bab07aa80e4fbab2d03c60a78e66b2773b7f65
Muhammad Noman <noman.siddique.306@gmail.com>
2021-05-30 13:50:42 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Center(child: Text("Login")),),
